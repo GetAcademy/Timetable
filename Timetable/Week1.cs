f1131487275eef579311377031c38ef5fcad9432
GET Academy <terje@kolderup.net>
2024-06-19 14:56:17 +0200

    Update Week1.cs

--- Timetable/Week1.cs
+++ Timetable/Week1.cs
@@ -3,13 +3,13 @@
     internal class Week1
     {
         private List<Session> _sessions;
-        private const int ColWidth = 20;
 
         public Week1()
         {
             _sessions = new List<Session>();
             _sessions.Add(new Session("Oppstart", 10, 0, 11, 30, DayOfWeek.Monday));
             _sessions.Add(new Session("Avslutning", 14, 0, 15, 0, DayOfWeek.Monday));
+            _sessions.Add(new Session("Chorei", 9, 0, 9, 30, DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Thursday, DayOfWeek.Friday));
             _sessions.Add(new Session("Undervisning", 10, 0, 11, 30, DayOfWeek.Tuesday));
             _sessions.Add(new Session("Undervisning", 12, 30, 14, 0, DayOfWeek.Thursday));
             _sessions.Add(new Session("Avslutning i team", 14, 45, 15, 00, DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday));
