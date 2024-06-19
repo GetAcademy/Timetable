0c54a0dc4a8ac239fe97773fb9bbabe7bd9309bd
GET Academy <terje@kolderup.net>
2024-06-19 14:18:02 +0200

    Initial commit

---
+++ Timetable/Session.cs
@@ -0,0 +1,18 @@
+﻿namespace Timetable
+{
+    internal class Session
+    {
+        private DayOfWeek[] _days;
+        private string _name;
+        private TimeOnly _start;
+        private TimeOnly _end;
+
+        public Session(string name, int startHour, int startMinute, int endHour, int endMinute, params DayOfWeek[] days)
+        {
+            _days = days;
+            _name = name;
+            _start = new TimeOnly(startHour, startMinute);
+            _end = new TimeOnly(endHour, endMinute);
+        }
+    }
+}
---
+++ Timetable/Week1.cs
@@ -0,0 +1,17 @@
+﻿namespace Timetable
+{
+    internal class Week1
+    {
+        private List<Session> _sessions;
+
+        public Week1()
+        {
+            _sessions = new List<Session>();
+            _sessions.Add(new Session("Oppstart", 10, 0, 11, 30, DayOfWeek.Monday));
+            _sessions.Add(new Session("Avslutning", 14, 0, 15, 00, DayOfWeek.Monday));
+            _sessions.Add(new Session("Undervisning", 10, 0, 11, 30, DayOfWeek.Tuesday));
+            _sessions.Add(new Session("Undervisning", 12, 30, 14, 00, DayOfWeek.Thursday));
+
+        }
+    }
+}
